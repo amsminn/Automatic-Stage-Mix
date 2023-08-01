ff230a13b08a2b643e2643ce9bc028900fc364a5
noname <48761044+noname0310@users.noreply.github.com>
2023-08-01 18:44:43 +0900

    some tests

--- premiere_plugin/src/index.ts
+++ premiere_plugin/src/index.ts
@@ -1,11 +1,18 @@
-import { PathUtil } from "./utils";
+// import { PathUtil } from "./utils";
 
-const projectPath = PathUtil.pathNormalize(app.project.path);
-const projectDir = projectPath.substring(0, projectPath.lastIndexOf("/") + 1);
+// const projectPath = PathUtil.pathNormalize(app.project.path);
+// const projectDir = projectPath.substring(0, projectPath.lastIndexOf("/") + 1);
 
-const filePath = projectDir + "test.txt";
-const file = new File(filePath);
-if (!file.open("r")) {
-    alert("file not found");
-}
-alert(file.read());
+// const filePath = projectDir + "test.txt";
+// const file = new File(filePath);
+// if (!file.open("r")) {
+//     alert("file not found");
+// }
+// alert(file.read());
+
+
+const project = app.project;
+const currentSequence = project.activeSequence;
+const selections = currentSequence.getSelection();
+$.bp();
+alert(selections.length);
