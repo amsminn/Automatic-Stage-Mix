21f239cc3c2c9ec13f3affa01c458daddee953c2
donggae0629 <122023282+donggae0629@users.noreply.github.com>
2023-08-07 20:59:44 +0900

    Update urop,test.py

--- urop,test.py
+++ urop,test.py
@@ -1,12 +1,43 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips, vfx
+from moviepy.editor import *
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+import random
+min_video_time = 10000
+change_time = 10
+set_clip = []
+pad_time = 4
+video_path = "C:/Users/aero0/Desktop/urop/영상 소스"
+origin_videopath = os.listdir(video_path)
+for i in range(len(origin_videopath)):
+    clip = os.path.join(video_path, sorted(os.listdir(video_path))[i])
+    clip = VideoFileClip(clip)
+    if min_video_time > clip.duration:
+        min_video_time = clip.duration
+    clip.subclip(0,clip.duration)
+    set_clip.append(clip)
+
+cur_video=0
+mix_clip = []
+t = 3
+mix_clip.append(set_clip[cur_video].subclip(0,min(t,min_video_time)))
+while t < min_video_time:
+    cur_t = t
+    next_t = min(t+change_time, min_video_time)
+    reference_clip = set_clip[cur_video].subclip(cur_t, next_t)
+    next_video = (cur_video + 1) % len(set_clip)
+    for video_idx in range(len(set_clip)):
+        if video_idx == cur_video:
+            continue
+        clip = set_clip[random.randrange(0,len(set_clip))].subclip(cur_t, next_t)
+        print(cur_video,cur_t)
+        t = next_t
+        mix_clip.append(clip.set_start(cur_t-1).crossfadein(1))
+    cur_video = next_video
+    # t = min(min_video_time, t + pad_time)
+    # pad_video = set_clip[cur_video].subclip(t,min(min_video_time, t + pad_time))
+    # mix_clip.append(pad_video)
+result_video = concatenate(mix_clip)
+result_video.write_videofile("real_final_video.mp4")
 
 
-def editvideo(videopath1,videopath2,first_stime,first_etime,second_stime,second_etime,fadetime,x1size,y1size,x2size,y2size):
-    clip1 = VideoFileClip(videopath1).subclip(first_stime,first_etime).fx(vfx.fadein,fadetime).fx(vfx.fadeout,fadetime)
-    clip2 = VideoFileClip(videopath2).subclip(second_stime,second_etime).fx(vfx.fadein,fadetime).fx(vfx.fadeout,fadetime)
-    clip2 = clip2.crop(x1=x1size, y1=y1size, x2=x2size, y2=y2size)
-    final_video = concatenate_videoclips([clip1,clip2])
-    final_video.write_videofile("final_video.mp4")
-    return final_video
-editvideo("C:/Users/aero0/Desktop/urop/one.mp4","C:/Users/aero0/Desktop/urop/two.mp4",30,49,49.01,80,0.01,0,0,0,0)
+
 
