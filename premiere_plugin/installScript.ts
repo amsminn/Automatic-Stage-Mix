0bed89f295e532695dc7845f6f612404d1ade0b1
noname <48761044+noname0310@users.noreply.github.com>
2023-08-10 19:55:53 +0900

    Update installScript.ts

--- premiere_plugin/installScript.ts
+++ premiere_plugin/installScript.ts
@@ -2,8 +2,6 @@ import fsextra from "fs-extra";
 
 const appData = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
 
-console.log(appData);
-
 const cilentSrc = "./client";
 const csxsSrc = "./CSXS";
 const hostSrc = "./host";
@@ -12,3 +10,5 @@ const installSrc = appData + "/Adobe/CEP/extensions/automatic-stage-mix";
 fsextra.copySync(cilentSrc, installSrc + "/client");
 fsextra.copySync(csxsSrc, installSrc + "/CSXS");
 fsextra.copySync(hostSrc, installSrc + "/host");
+
+console.log(`installed to ${installSrc}`);
